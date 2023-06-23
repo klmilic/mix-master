7135a91893806b9baf9f589ac527a030388c4734
Kirsten Milic <kirsten.milic@gmail.com>
2023-06-23 13:54:43 -0700

    remove headers

--- webpack.config.js
+++ webpack.config.js
@@ -28,12 +28,6 @@ module.exports = {
         
       },
     },
-    headers: {
-      'Access-Control-Allow-Origin': 'http://localhost:8080',
-      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
-      'Access-Control-Allow-Headers':
-        'X-Requested-With, content-type, Authorization',
-    },
     historyApiFallback: true,
   },
   module: {
